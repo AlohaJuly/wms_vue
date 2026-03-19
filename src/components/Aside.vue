8df24aa090b723992a1db28d29ef0b77e6b0df49
AlohaJuly <1692883952@qq.com>
2026-03-19 15:39:36 +0800

    完成新增用户信息校验

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,36 +1,35 @@
-<template>
-  <el-menu
-    active-text-color="#ffd04b"
-    background-color="#545c64"
-    text-color="#fff"
-    default-active="/Home"
-    :collapse="isCollapse"
-    :collapse-transition="false"
-    style="height: 100vh;"
-  >
-    <el-menu-item index="/Home">
-      <el-icon><House /></el-icon>
-      <span>首页</span>
-    </el-menu-item>
+ <template>
+    <el-menu 
+      active-text-color="#ffd04b"
+      background-color="#545c64"
+      text-color="#fff"
+      default-active="/Home"
+      :collapse="isCollapse"
+      collaps-transition="false"
+      style="height: 100vh;">
+      <el-menu-item index="/Home">
+        <el-icon><House /></el-icon>
+        <span>首页</span>
+      </el-menu-item>
 
-    <el-menu-item index="/Navigation1">
-      <el-icon><Soccer /></el-icon>
-      <span>导航一</span>
-    </el-menu-item>
+      <el-menu-item index="/Navigation1">
+        <el-icon><Soccer /></el-icon>
+        <span>导航一</span>
+      </el-menu-item>
 
-    <el-menu-item index="/Navigation2">
-      <el-icon><Football /></el-icon>
-      <span>导航二</span>
-    </el-menu-item>
-  </el-menu>
+      <el-menu-item index="/Navigation2">
+        <el-icon><Football /></el-icon>
+        <span>导航二</span>
+      </el-menu-item>
+    </el-menu>
 </template>
 
 <script lang="ts" setup>
+import { ElMenuItem } from 'element-plus';
 import { House } from '@element-plus/icons-vue'
 import { Football } from '@element-plus/icons-vue'
 import { Soccer } from '@element-plus/icons-vue'
 
-defineProps<{
-  isCollapse: boolean
-}>()
+const isCollapse = false
 </script>
+ 
